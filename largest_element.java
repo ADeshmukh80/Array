a6d5412487ea58c86418e911585f4f27db301aaa
Anisha Deshmukh <123193995+ADeshmukh80@users.noreply.github.com>
2024-06-04 01:09:16 +0530

    Add files via upload

---
+++ largest_element.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class largest_element {
+    public static int largest(int arr[], int n) {
+        int max = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = scanner.nextInt();
+        
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        System.out.println("Largest in given array is " + largest(arr, n));
+
+        scanner.close(); 
+    }
+}
